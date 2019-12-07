57b48dfb8e3c58d79d529a39f753aa2fc81bee54
Jürgen Dressel <juegge@murks.at>
2019-12-07 18:36:08 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+   entry: './src/index.ts',
+   module: {
+		rules: [
+			{
+					test: /\.(ts|tsx)$/,
+					use: [{
+						loader: 'ts-loader',
+					}],
+					exclude: /node-modules/,
+					
+         },
+         {
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: 'javascript/auto'
+         },
+			{
+					test:/\.(s*)css$/,
+					use:['style-loader',
+						{
+							loader: 'css-loader'
+						},
+						{
+							loader: 'sass-loader'
+						}]
+			}
+		]
+   },
+   resolve: {
+		extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx']
+   },
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+   }
+}
